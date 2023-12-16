c23088ceb33c701e86887bdf899da032ffb280b0
Rodey18 <raihky.main@gmail.com>
2023-12-16 22:47:46 +0800

    Update

--- src/pages/publikasi/PublikasiFile.tsx
+++ src/pages/publikasi/PublikasiFile.tsx
@@ -1,43 +1,52 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { IonCard, IonHeader, IonCardContent, IonProgressBar, IonAlert, IonPage, IonButton } from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonCard, IonHeader, IonCardContent, IonPage, IonButton } from '@ionic/react';
 import MenuSlide from '../../components/menu-Slide/menuSlide';
 import Toolbar from '../../components/toolbar/toolbar';
+import { db, storage } from '../firebase/firebase';
+import { collection, addDoc } from 'firebase/firestore';
+import { ref, uploadBytes } from 'firebase/storage';
 import './PublikasiFile.css';
 
 const Publikasi: React.FC = () => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [buffer, setBuffer] = useState<number>(0);
-  const [progress, setProgress] = useState<number>(0);
-  const [showAlert, setShowAlert] = useState<boolean>(false);
-  const [showProgress, setShowProgress] = useState<boolean>(false);
 
-  const handleUpload = async (event: React.FormEvent) => {
-    event.preventDefault();
-    if (fileInputRef.current?.files?.length) {
-      const file = fileInputRef.current.files[0] as File;
-      console.log(file);
-      setProgress(0);
-      setBuffer(10);
-      setShowProgress(true);
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newYear, setNewYear] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+  const [newTags, setNewTags] = useState("");
+  const [newDoc, setNewDoc] = useState<File | null>(null);
+  const [inputKey, setInputKey] = useState(Date.now());
 
-      setTimeout(() => {
-        setShowAlert(true);
-        setProgress(0);
-        setBuffer(0);
-        setShowProgress(false);
-      }, 20000);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const userCollectionRef = collection(db, 'documents')
+
+  const createData = async () => {
+    if (newDoc == null || newAuthor == "" || Number(newYear) === 0 || newTitle == "") return;
+    setIsLoading(true);
+    try {
+      await addDoc(userCollectionRef, { author: newAuthor, year: newYear, title: newTitle, tags: newTags });
+      const docRef = ref(storage, `documents/${newAuthor + "-" + newTitle}`);
+      await uploadBytes(docRef, newDoc);
+      alert("Dokumen Terupload");
+
+      setNewAuthor("");
+      setNewYear("");
+      setNewTitle("");
+      setNewTags("");
+      setNewDoc(null);
+      setInputKey(Date.now());
+
+    } catch (error) {
+      console.error(error);
+      alert(collection(db, 'documents'));
     }
-  };
+
+    setIsLoading(false);
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setProgress((prevProgress) => prevProgress + 0.05);
-      setBuffer((prevBuffer) => prevBuffer + 0.05);
-    }, 1000);
-
-    return () => clearInterval(interval);
+    console.log(userCollectionRef)
   }, []);
-
   return (
     <>
       <MenuSlide />
@@ -51,47 +60,40 @@ const Publikasi: React.FC = () => {
         </IonHeader>
         <IonCard className="publikasi">
           <IonCardContent className="bagian">
-            <form onSubmit={handleUpload}>
-              <div style={{ height: '42px' }}>
-                <h1 style={{ width: '100%', textAlign: 'center', fontWeight: "500", margin: '0px' }}>UNGGAH DOKUMEN</h1>
-              </div>
-              <div className='input-container'>
-                <p className='labels'>Author <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
-                <input className='input-box' type="text" placeholder="  Masukkan nama-nama penulis..." />
-              </div>
-              <div className='input-container'>
-                <p className='labels'>Year <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
-                <input className='input-box' type="text" placeholder="  Masukkan tahun..." />
-              </div>
-              <div className='input-container'>
-                <p className='labels'>Judul <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
-                <input className='input-box' type="text" placeholder="  Masukkan judul..." />
-              </div>
-              <div className='input-container'>
-                <p className='labels'>Kata Kunci</p>
-                <input className='input-box' type="text" placeholder="  Masukkan kata kunci untuk dokumen..." />
-              </div>
-              <div className='input-container'>
-                <p className='labels'>File <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
-                <div id='file-box'>
-                  <input type="file" ref={fileInputRef} />
-                  <button id='upload-button' type="submit">
-                    <p style={{ fontWeight: '500', fontSize: '12px', margin: '0px', color: 'white' }}>UPLOAD</p>
-                  </button>
-                </div>
-                {showProgress && <IonProgressBar buffer={buffer} value={progress} />}
-              </div>
-            </form>
-            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '12px' }}>
-              <IonButton id='submit-button'>KIRIM</IonButton>
+            <div style={{ height: '42px' }}>
+              <h1 style={{ width: '100%', textAlign: 'center', fontWeight: "500", margin: '0px' }}>UNGGAH DOKUMEN</h1>
             </div>
-            <IonAlert
-              isOpen={showAlert}
-              onDidDismiss={() => setShowAlert(false)}
-              header={'Upload Complete'}
-              message={'Your file has been successfully uploaded.'}
-              buttons={['OK']}
-            />
+            <div className='input-container'>
+              <p className='labels'>Author <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
+              <input className='input-box' value={newAuthor} onChange={(e) => setNewAuthor(e.target.value)} type="text" placeholder="  Masukkan nama-nama penulis..." />
+            </div>
+            <div className='input-container'>
+              <p className='labels'>Year <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
+              <input className='input-box' value={newYear} onChange={(e) => setNewYear(e.target.value)} type="number" placeholder="  Masukkan tahun..." />
+            </div>
+            <div className='input-container'>
+              <p className='labels'>Judul <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
+              <input className='input-box' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} type="text" placeholder="  Masukkan judul..." />
+            </div>
+            <div className='input-container'>
+              <p className='labels'>Kata Kunci</p>
+              <input className='input-box' value={newTags} onChange={(e) => setNewTags(e.target.value)} type="text" placeholder="  Masukkan kata kunci untuk dokumen..." />
+            </div>
+            <div className='input-container'>
+              <p className='labels'>File <span style={{ color: 'rgb(223, 66, 66)', fontSize: '12px !important' }}>*</span></p>
+              <div id='file-box'>
+                <input key={inputKey} type="file" onChange={(e) => {
+                  if (e.target.files) {
+                    setNewDoc(e.target.files[0]);
+                  }
+                }} />
+              </div>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', marginTop: '12px' }}>
+              <IonButton id='submit-button' onClick={createData} disabled={isLoading}>
+                {isLoading ? "Sedang Mengirim..." : "KIRIM"}
+              </IonButton>
+            </div>
           </IonCardContent>
         </IonCard>
       </IonPage>
